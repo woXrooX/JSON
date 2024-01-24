d6047080082e270a7d2ca64a7427cc4649aedd9b
woXrooX <woXrooX@gmail.com>
2024-01-24 21:05:52 +0100

    Clean up

--- source/main.cpp
+++ source/main.cpp
@@ -3,28 +3,40 @@
 #include "json.hpp"
 
 int main(){
-  std::string data = R"(
-{
-  "int": 5,
-  "str": "Some Text",
-  "arr": [1, 2, 3, "a", "b"],
-  "obj": {
-    "child": false
-  }
-}
+
+  std::string sampleJSON = R"(
+    {
+      "int": 5,
+      "str": "Some Text",
+      "arr": [1, 2, 3, "a", "b"],
+      "obj": {
+        "child": false
+      }
+    }
   )";
 
+  ///// Examples
+  // tokenizer
+  // woXrooX::Tokenizer t("./source/test.json");
+
+  // lexer
+  woXrooX::Lexer::init(sampleJSON);
+
+
+
+  // JSON
   // std::string a = "aaa";
-  // woXrooX::Json b;
-  // woXrooX::Json j;
+  // woXrooX::JSON b;
+  // woXrooX::JSON j;
   // j = b;
-  // j.setRealNumber(5);
-  // std::cout << j.getRealNumber() << '\n';
-  // std::cout << "\nSize: " << sizeof(j) << '\n';
+  // b = 5;
+  // std::cout << b.getRealNumber() << '\n';
+  // std::cout << "\nSize: " << sizeof(b) << '\n';
 
+  // woXrooX::JSON a, b;
 
-  woXrooX::Tokenizer t("./source/test.json");
+  // a["a"] = 10.5;
+  // std::cout << a["a"].getRealNumber() << '\n';
 
   return 0;
-
 }
